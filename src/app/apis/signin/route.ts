b29fb61fe45baf238bfd696b68642a5feba8fbc0
Bang Junwoo <bang.jun@sidewalkplay.com>
2023-09-20 14:41:24 +0900

    CORS

--- src/app/apis/signin/route.ts
+++ src/app/apis/signin/route.ts
@@ -71,5 +71,10 @@ export async function POST(request: Request) {
 
   return NextResponse.json(data, {
     status,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
   })
 }
